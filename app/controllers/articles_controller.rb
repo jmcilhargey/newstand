256b4976691f59e82eaddd806a36e0a3956d093f
codecademy <ccuser@codecademy.com>
2015-12-23 01:07:59 -0500

    Added controller action in sessions for user login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   
-  before_action :require_user, except: :index
+  before_action :current_user, except: :index
   
   def index
     @articles = Article.all
