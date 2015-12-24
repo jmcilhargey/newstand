d8c7aea98d9c1305ffa8ecb15a65a0a2d767e3e3
codecademy <ccuser@codecademy.com>
2015-12-23 20:19:45 -0500

    Fixed application controller actions for user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   
   def current_user
-    @current_user ||= User.find(session[:userid]) if session[:userid]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   def require_user
-    redirect_to '/login' unless @current_user
+    redirect_to '/login' unless current_user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20151223034139) do
   create_table "articles", force: true do |t|
     t.string   "title"
     t.string   "author"
-    t.text   "body"
+    t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
